7a674602bef811298c7b013e65a86e3dde04ba49
Charlie Iorg <ciorg@terascope.io>
2018-08-02 10:37:41 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# teraslice-state-storage
-State storage module for Teraslice. Currently experimental
+# teraslice state storage
+
+Module to save state for teraslice processes.  Currently uses elasticsearch for state persistance and lru-caching to reduce network calls.  Requires job context for connection details and config object which is passed from the _op in the teraslice job.
+
+## Intallation and Usage
+### Installation
+> Installation TBD
+### Usage
+> `const storage = require('teraslice-state-storage');`  
+> `storage.count();`  
+> `storage.mset(docArray);`  
+
+## Configuration Options:
+  * __cache_limit__ - max number of items to store in the cache (not memory size)  
+  positive integer, default: 1000000
+
+  * __connection__ - elasticsearch connection  
+   string, default: 'default'
+
+  * __index__ - name of elasticsearch index  
+  string, default: 'index'
+
+  * __type__ - type of the elasticsearch data
+  string, default: 'type'
+
+  * __chunk_size__ - how many docs to send in the elasticsearch mget request at a time  
+  postitive integer, default is 2500
+
+  * __concurrency__ - number of cuncurrent requests to elasticsearch  
+  positive integer, default: 100
+
+  * __source_fields__ - fields to retreive from elasticsearch  
+  array of fields, defaults to all fields
+
+  * __id_field__ - specifies the field to use as the key for caching and retrieving docs from elasticsearch  
+  string, default: 'id'
+  
+  * __persist__ - If set to true will save state in storage for set or mset.  
+  boolean, default: false
+
+## Functions:
+* __get__ - expects an object with the `id_field` as a property and returns an object with an id and saved fields  
+  example: `get(doc)`
+
+* __mget__ - expects an array of objects and returns an object  
+  example: `mget(docArray)`
+
+* __set__ - expects and object, boolean is optional. Updates state with an option to save state in storage  
+  example: `set(doc)`  
+  example: `set(doc, true)` - saves the state in storage
+
+* __mset__ - expects an array of objects boolean is optional.  Updates state with an option to save state in storage.  
+  example: `mset(docArray)`  
+  example: `mset(docArray, true)` - saves the state in storage
+
+* count - returns the number docs currently saved in the cache  
+  example: `count()`
+
---
+++ spec/es-backed-state-storage-spec.js
@@ -0,0 +1,134 @@
+'use strict';
+
+const _ = require('lodash');
+const StateStorage = require('../lib/es-cache-state-storage');
+
+let mgetData;
+let getData;
+let indexData;
+let bulkData;
+
+const context = {
+    foundation: {
+        apis: {
+            elasticsearch: () => ({
+                mget: () => Promise.resolve(mgetData),
+                get: () => Promise.resolve(getData),
+                indexWithId: (request) => {
+                    indexData = request;
+                    return Promise.resolve();
+                }
+            })
+        },
+        getConnection: () => ({
+            client: {
+                bulk: (request) => {
+                    bulkData = request;
+                    return Promise.resolve();
+                }
+            }
+        })
+    }
+};
+
+
+function createMgetData(dataArray) {
+    return dataArray.map((item) => {
+        const doc = {
+            _index: 'index',
+            _type: 'type',
+            _id: item.id,
+            _version: 1,
+            found: true,
+            _source: item
+        };
+
+        return doc;
+    });
+}
+
+const doc = {
+    id: 1,
+    data: 'thisIsSomeData'
+};
+
+
+const docArray = [
+    {
+        id: '1',
+        data: 'thisIsSomeData'
+    },
+    {
+        id: '2',
+        data: 'thisIsSomeData'
+    },
+    {
+        id: '3',
+        data: 'thisIsLotsOfData'
+    }
+];
+
+const config = {};
+
+describe('es backed cache', () => {
+    it('set should update storage for doc', () => {
+        const stateStorage = new StateStorage(context, config);
+        stateStorage.set(doc);
+        expect(stateStorage.count()).toBe(1);
+    });
+
+    it('set should update storage for doc and persist doc if specified', () => {
+        config.persist = true;
+        const stateStorage = new StateStorage(context, config);
+        stateStorage.set(doc, true);
+        expect(stateStorage.count()).toBe(1);
+        expect(indexData.body).toEqual({ id: 1, data: 'thisIsSomeData' });
+    });
+
+    it('mset should update storage for many docs and persist doc if specified', () => {
+        config.persist = true;
+        const stateStorage = new StateStorage(context, config);
+        return stateStorage.mset(docArray)
+            .then(() => {
+                expect(stateStorage.count()).toBe(3);
+                // bulk requests double the array size
+                expect(bulkData.body.length).toBe(6);
+            });
+    });
+
+    it('get should return doc state', () => {
+        const stateStorage = new StateStorage(context, config);
+        stateStorage.set(doc);
+        return stateStorage.get(doc)
+            .then(result => expect(result).toEqual({ id: 1, data: 'thisIsSomeData' }));
+    });
+
+    it('get should return doc state even if not in cache', () => {
+        const stateStorage = new StateStorage(context, config);
+        getData = { id: '1', data: 'thisIsSomeData' };
+        return stateStorage.get(doc)
+            .then(result => expect(result).toEqual({ id: '1', data: 'thisIsSomeData' }));
+    });
+
+    it('mget should return doc state for docs in cache and es', () => {
+        config.persist = true;
+        const stateStorage = new StateStorage(context, config);
+        mgetData = {
+            docs: createMgetData(docArray.slice(0, 1))
+        };
+        return stateStorage.mset(docArray.slice(1, 3))
+            .then(() => stateStorage.mget(docArray))
+            .then((result) => {
+                expect(_.keys(result).length).toBe(3);
+                expect(result['1']).toEqual({ id: '1', data: 'thisIsSomeData' });
+            });
+    });
+
+    it('dedupe removes docs with duplicate keys', () => {
+        const doubleDocs = docArray.concat(_.cloneDeep(docArray));
+        const stateStorage = new StateStorage(context, config);
+        const deduped = stateStorage._dedupeDocs(doubleDocs);
+        expect(doubleDocs.length).toBe(6);
+        expect(deduped.length).toBe(3);
+    });
+});
